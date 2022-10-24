9dc7531775dd9e49ec794cc3a6e4a6e22ad25e3f
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-10-24 11:15:41 -0700

    first

--- MyScanner.java
+++ MyScanner.java
@@ -4,7 +4,6 @@ public class MyScanner
 {
     public void run() {
         Scanner scanner = new Scanner(System.in);
-        scanner.useDelimiter(",|\n");
         while (true) {
             String word = scanner.next();
             System.out.println(word);
